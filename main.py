764b31fc003e0f3a86d61227339a4dba5b693cff
3511 <Brev4ik@gmail.com>
2026-03-12 19:05:27 +0200

    i add command print

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("hello all world")
+class Human:
+    height1 = 180
+    height2 = 190
+class Student(Human):
+    pass
+class Worker(Human):
+    pass
+
+nick = Student()
+jon = Worker()
+print(f"Висота {nick.height1}")
+print(f"Висота {jon.height2}")
